b0705b1b3e44bf69b5a7f97b4d4fcbed9f846764
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-07-25 14:43:38 -0300

    chore: ignore binary files

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,38 @@
 #![allow(non_snake_case)]
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 
 use std::collections::HashMap;
-// use std::collections::BTreeMap;
+use std::collections::BTreeMap;
 
 use std::error::Error;
 use std::result::Result;
 
+mod parse_csv;
+use parse_csv::readTypesCsv;
+
+const TYPES_NYC_PATH: &str = "resources/Dataset Alumnos/typesNYC.csv";
+const TYPES_CHI_PATH: &str = "resources/Dataset Alumnos/typesCHI.csv";
 // -> Result<()>
+
+
 // means it may return an empty tuple () if everything's alright, or an error if something went wrong
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut typesByAcronym: HashMap<String, String> = HashMap::new();
-    // let typesByAgencyBySize: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
 
-    readTypesCsvNyc(&mut typesByAcronym)?;
+    // maps for chi
+    let mut typesByAcronymCHI: HashMap<String, String> = HashMap::new();
+    let typesByAgencyBySizeCHI: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
+    // maps for nyc
+    let mut typesByAcronymNYC: HashMap<String, String> = HashMap::new();
+    let typesByAgencyBySizeNYC: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
+    
+    //first we read types
+    readTypesCsv(TYPES_NYC_PATH,&mut typesByAcronymCHI)?;
+    readTypesCsv(TYPES_CHI_PATH,&mut typesByAcronymNYC)?;
 
-    typesByAcronym.iter().for_each(|f| println!("{:?}", f));
 
-    Ok(())
-}
-
-fn readTypesCsvNyc(typesByAcronym: &mut HashMap<String, String>) -> Result<(), Box<dyn Error>> {
-    let root = std::env::current_dir()?;
-    let path = root.join("resources/Dataset Alumnos/typesNYC.csv");
-
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    for (i, line) in reader.lines().enumerate() {
-        if i == 0 {
-            continue;
-        }
-
-        let line = line?;
-
-        let fields: Vec<&str> = line.split(';').collect();
-        typesByAcronym.insert(fields[0].to_string(), fields[1].to_string()); // to_string turns them into Strings (owned), instead of &str (borrowed, same lifetime as `line`)
-    }
+    typesByAcronymNYC.iter().for_each(|f| println!("{:?}", f));
 
     Ok(())
 }
+
+

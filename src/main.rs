039afb236e2472de0c31647cd7de1c7ada49eb26
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-07-25 15:57:31 -0300

    feat: readTypesCsv, parse_csv.rs
    Co-Authored-By: Pedro Gonzalez Nuñez <81493265+pedrogonzaleznunez@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,58 @@
 #![allow(non_snake_case)]
 
-use std::collections::HashMap;
 use std::collections::BTreeMap;
+use std::collections::HashMap;
 
 use std::error::Error;
 use std::result::Result;
 
 mod parse_csv;
+use parse_csv::readReqCsv;
 use parse_csv::readTypesCsv;
 
 const TYPES_NYC_PATH: &str = "resources/Dataset Alumnos/typesNYC.csv";
 const TYPES_CHI_PATH: &str = "resources/Dataset Alumnos/typesCHI.csv";
-// -> Result<()>
 
+// just a million baby
+const REQ_NYC_PATH: &str = "resources/Dataset Alumnos/million/requestsNYC.csv";
+const REQ_CHI_PATH: &str = "resources/Dataset Alumnos/million/requestsCHI.csv";
+
+#[cfg(all(feature = "nyc", feature = "chi"))]
+compile_error!("Only one of `nyc` or `chi` features can be enabled");
+
+#[cfg(not(any(feature = "nyc", feature = "chi")))]
+compile_error!("`nyc` or `chi` must be selected");
 
 // means it may return an empty tuple () if everything's alright, or an error if something went wrong
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     // maps for chi
     let mut typesByAcronymCHI: HashMap<String, String> = HashMap::new();
-    let typesByAgencyBySizeCHI: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
+    let mut typesByAgencyBySizeCHI: BTreeMap<String, BTreeMap<String, i32>> = BTreeMap::new();
+
     // maps for nyc
     let mut typesByAcronymNYC: HashMap<String, String> = HashMap::new();
-    let typesByAgencyBySizeNYC: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
-    
+    let mut typesByAgencyBySizeNYC: BTreeMap<String, BTreeMap<String, i32>> = BTreeMap::new();
+
     //first we read types
-    readTypesCsv(TYPES_NYC_PATH,&mut typesByAcronymCHI)?;
-    readTypesCsv(TYPES_CHI_PATH,&mut typesByAcronymNYC)?;
+    readTypesCsv(
+        TYPES_NYC_PATH,
+        &mut typesByAcronymNYC,
+        &mut typesByAgencyBySizeNYC,
+    )?;
+    // typesByAcronymNYC.iter().for_each(|f| println!("{:?}", f));
+    // readTypesCsv(TYPES_CHI_PATH, &mut typesByAcronymNYC, &mut typesByAgencyBySizeCHI)?;
 
+    readReqCsv(
+        REQ_NYC_PATH,
+        &mut typesByAcronymNYC,
+        &mut typesByAgencyBySizeNYC,
+    )?;
+    // readReqCsv(REQ_CHI_PATH, &mut typesByAcronymCHI, &mut typesByAgencyBySizeCHI)?;
 
-    typesByAcronymNYC.iter().for_each(|f| println!("{:?}", f));
+    typesByAgencyBySizeNYC
+        .iter()
+        .for_each(|(a, b)| b.iter().for_each(|(k, v)| println!("{}", v)));
 
     Ok(())
 }
-
-
---
+++ src/parse_csv.rs
@@ -0,0 +1,108 @@
+#![allow(non_snake_case)]
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use std::collections::BTreeMap;
+use std::collections::HashMap;
+
+use std::error::Error;
+use std::result::Result;
+
+//constants for CSV files
+// for CHI
+enum ChiFields {
+    Acronym = 0,
+    AgencyName = 1,
+    Status = 2,
+    CreatedDate = 3, // yyyy-MM-dd HH:mm:ss
+    BoroughName = 4,
+    Latitude = 5,
+    Longitude = 6,
+}
+// for NYC
+enum NycFields {
+    CreatedDate = 0, // yyyy-MM-dd HH:mm:ss
+    AgencyName = 1,
+    Acronym = 2,
+    Status = 3,
+    Borough = 4,
+    Latitude = 5,
+    Longitude = 6,
+}
+enum TypesFields {
+    Name = 0,
+    Acronym = 1,
+}
+
+pub fn readTypesCsv(
+    filePath: &str,
+    typesByAcronym: &mut HashMap<String, String>,
+    typesByAgencyBySize: &mut BTreeMap<String, BTreeMap<String, i32>>,
+) -> Result<(), Box<dyn Error>> {
+    let root = std::env::current_dir()?;
+    let path = root.join(filePath);
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    for (i, line) in reader.lines().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        let line = line?;
+
+        let fields: Vec<&str> = line.split(';').collect();
+
+        //add key and value to typesByAcronym map
+        typesByAcronym.insert(
+            fields[TypesFields::Acronym as usize].to_string(),
+            fields[TypesFields::Name as usize].to_string(),
+        );
+        // add just key to typesByAgencyBySize map
+        typesByAgencyBySize.insert(
+            fields[TypesFields::Acronym as usize].to_string(),
+            BTreeMap::new(),
+        );
+    }
+
+    Ok(())
+}
+
+pub fn readReqCsv(
+    filePath: &str,
+    typesByAcronym: &mut HashMap<String, String>,
+    typesByAgencyBySize: &mut BTreeMap<String, BTreeMap<String, i32>>,
+) -> Result<(), Box<dyn Error>> {
+    let root = std::env::current_dir()?;
+    let path = root.join(filePath);
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    for (i, line) in reader.lines().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        let line = line?;
+
+        let fields: Vec<&str> = line.split(';').collect();
+
+        let agencyName: &str = fields[NycFields::AgencyName as usize];
+        let acronym: &str = fields[NycFields::Acronym as usize];
+        let typeName: &str = typesByAcronym
+            .get(acronym)
+            .expect("Type name must be defined");
+
+        typesByAgencyBySize
+            .entry(typeName.to_string())
+            .or_insert_with(BTreeMap::new)
+            .entry(agencyName.to_string())
+            .and_modify(|count: &mut i32| *count += 1)
+            .or_insert(1);
+    }
+
+    Ok(())
+}

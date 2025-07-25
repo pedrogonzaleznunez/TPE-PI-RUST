d45b037475438f2ada6d121c79ae8cec4f1f44e1
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-07-25 14:14:51 -0300

    feat: readTypesCsvNyc, insert into mut HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,44 @@
 #![allow(non_snake_case)]
 use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::io::{BufRead, BufReader};
 
 use std::collections::HashMap;
-use std::collections::BTreeMap;
+// use std::collections::BTreeMap;
 
-use std::option;
+use std::error::Error;
+use std::result::Result;
 
 // -> Result<()>
-// significa que puede devolver una tupla vacia () si todo sale bien, o un error si algo falla
+// means it may return an empty tuple () if everything's alright, or an error if something went wrong
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut typesByAcronym: HashMap<String, String> = HashMap::new();
+    // let typesByAgencyBySize: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
 
-    let typesByAcronym: HashMap<String, String> = HashMap::new();
-    let typesByAgencyBySize: BTreeMap<String, BTreeMap<String, String>> = BTreeMap::new();
-    // Abrir el archivo
-    let root = std::env::current_dir()?;
-    let path = root.join("resources/Dataset Alumnos/typesCHI.csv");
+    readTypesCsvNyc(&mut typesByAcronym)?;
 
-    println!("{}", path.display());
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    for (i, line) in reader.lines().enumerate() {
-        let line = line?; // obtengo la linea como un Result<String>
-        let fields: Vec<&str> = line.split(',').collect();
-
-        if i == 0 {
-            println!("{:?}", fields);
-            println!("{}", '\n');
-        } else {
-            println!("{} {:?}", i, fields);
-        }
-    }
+    typesByAcronym.iter().for_each(|f| println!("{:?}", f));
 
     Ok(())
 }
 
-fn readTypesCsvNyc(typesByAcronym: &HashMap<String, String> ) -> Option<i32> {
-
-    let root = std::env::current_dir().ok()?;
+fn readTypesCsvNyc(typesByAcronym: &mut HashMap<String, String>) -> Result<(), Box<dyn Error>> {
+    let root = std::env::current_dir()?;
     let path = root.join("resources/Dataset Alumnos/typesNYC.csv");
 
-    let file = File::open(path).ok()?;
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
 
     for (i, line) in reader.lines().enumerate() {
-        
         if i == 0 {
             continue;
         }
 
-        let line = line.ok()?;
+        let line = line?;
 
         let fields: Vec<&str> = line.split(';').collect();
-
-        println!();
+        typesByAcronym.insert(fields[0].to_string(), fields[1].to_string()); // to_string turns them into Strings (owned), instead of &str (borrowed, same lifetime as `line`)
     }
 
-    Some(42)
-} 
+    Ok(())
+}

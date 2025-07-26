615005c96928cd9c382a418d26eabc87fc8ed4cc
Pedro Gonzalez Nuñez <81493265+pedrogonzaleznunez@users.noreply.github.com>
2025-07-26 10:51:36 -0300

    feat(query2): first working version of query2
    - Renamed readReqCsv to readReqCsvV1 to clarify query separation
    - Implemented readReqCsvV2 completely
    - Added basic output for Q2 in main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,13 @@ mod constants;
 mod parse_csv;
 
 use constants::CITY_CONFIG;
-use parse_csv::readReqCsv;
 use parse_csv::readTypesCsv;
+
+use parse_csv::readReqCsvQ1;
+use parse_csv::readReqCsvQ2;
+// use parse_csv::readReqCsvQ3;
+// use parse_csv::readReqCsvQ4;
+// use parse_csv::readReqCsvQ5;
 
 #[cfg(all(feature = "nyc", feature = "chi"))]
 compile_error!("Only one of `nyc` or `chi` features can be enabled");
@@ -20,25 +25,39 @@ compile_error!("Only one of `nyc` or `chi` features can be enabled");
 compile_error!("`nyc` or `chi` must be selected");
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // structures for query 1
     let mut typesByAcronym: HashMap<String, String> = HashMap::new();
     let mut typesByAgencyBySize: BTreeMap<String, BTreeMap<String, i32>> = BTreeMap::new();
 
+    // structures for query 2
+    let mut boroughLatLngBySize: BTreeMap<(String, i32, i32), i32> = BTreeMap::new();
+
+    // QUERY 1 - read csv files
     readTypesCsv(
         CITY_CONFIG.typesFilePath,
         &mut typesByAcronym,
         &mut typesByAgencyBySize,
     )?;
 
-    readReqCsv(
+    readReqCsvQ1(
         CITY_CONFIG.requestsFilePath,
         &mut typesByAcronym,
         &mut typesByAgencyBySize,
     )?;
 
-    typesByAgencyBySize.iter().for_each(|(infr, b)| {
-        b.iter()
-            .for_each(|(agency, v)| println!("{} ({}) - {}", infr, agency, v))
-    });
+    // QUERY 2 - read csv files
+    readReqCsvQ2(CITY_CONFIG.requestsFilePath, &mut boroughLatLngBySize)?;
+
+    // print for query1
+    // typesByAgencyBySize.iter().for_each(|(infr, b)| {
+    //     b.iter()
+    //         .for_each(|(agency, v)| println!("{} ({}) - {}", infr, agency, v))
+    // });
+
+    // print for query2
+    boroughLatLngBySize
+        .iter()
+        .for_each(|((borough, lat, lng), v)| println!("{};{};{};{}", borough, lat, lng, v));
 
     Ok(())
 }
--- src/parse_csv/mod.rs
+++ src/parse_csv/mod.rs
@@ -51,7 +51,7 @@ pub fn readTypesCsv(
     Ok(())
 }
 
-pub fn readReqCsv(
+pub fn readReqCsvQ1(
     filePath: &str,
     typesByAcronym: &mut HashMap<String, String>,
     typesByAgencyBySize: &mut BTreeMap<String, BTreeMap<String, i32>>,
@@ -87,4 +87,57 @@ pub fn readReqCsv(
     }
 
     Ok(())
+}
+
+pub fn readReqCsvQ2(
+    filePath: &str,
+    boroughLatLngBySize: &mut BTreeMap<(String, i32, i32), i32>,
+) -> Result<(), Box<dyn Error>> {
+    let root = std::env::current_dir()?;
+    let path = root.join(filePath);
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    for (i, line) in reader.lines().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        let line = line?;
+
+        let fields: Vec<&str> = line.split(';').collect();
+
+        //collect data from constants
+        let borough: &str = fields[CITY_CONFIG.requestCSVFields.Borough as usize];
+
+        let lat: f64 = fields[CITY_CONFIG.requestCSVFields.Latitude as usize]
+            .parse()
+            .expect("Latitude must be a valid number");
+
+        let lng: f64 = fields[CITY_CONFIG.requestCSVFields.Longitude as usize]
+            .parse()
+            .expect("Longitude must be a valid number");
+
+        // get the cuadrant from lat and lng
+        let (lat_cuadrant, lng_cuadrant) = getCuadrantFromLatLng(lat, lng);
+
+        // now we can insert the data into the map
+        // the key is a tuple of (borough, lat_cuadrant, lng_cuadrant)
+        // and the value is the count of requests in that cuadrant
+        boroughLatLngBySize
+            .entry((borough.to_string(), lat_cuadrant, lng_cuadrant))
+            .and_modify(|count: &mut i32| *count += 1)
+            .or_insert(1);
+    }
+
+    Ok(())
+}
+
+// aux method for query 2
+fn getCuadrantFromLatLng(lat: f64, lng: f64) -> (i32, i32) {
+    let lat_cuadrant = (lat / 0.1).floor() as i32;
+    let lng_cuadrant = (lng / 0.1).floor() as i32;
+
+    (lat_cuadrant, lng_cuadrant)
 }
